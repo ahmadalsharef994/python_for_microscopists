ab3384d2d591fdad43a695da4e3a3caff389de7c
ahmadalsharef994 <51163921+ahmadalsharef994@users.noreply.github.com>
2021-11-29 13:06:04 +0530

    Update simple_3d_unet.py

--- 231_234_BraTa2020_Unet_segmentation/simple_3d_unet.py
+++ 231_234_BraTa2020_Unet_segmentation/simple_3d_unet.py
@@ -6,10 +6,10 @@ Converted my standard 2D Unet to 3D.
 
 """
 
-from keras.models import Model
-from keras.layers import Input, Conv3D, MaxPooling3D, concatenate, Conv3DTranspose, BatchNormalization, Dropout, Lambda
-from keras.optimizers import Adam
-from keras.metrics import MeanIoU
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Conv3D, MaxPooling3D, concatenate, Conv3DTranspose, BatchNormalization, Dropout, Lambda
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.metrics import MeanIoU
 
 kernel_initializer =  'he_uniform' #Try others if you want
 
